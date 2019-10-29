6f0725cdb20c220018a095ed246d3b657398cde1
xishiharu126x <ishiharu126@gmail.com>
2019-10-29 11:49:56 +0800

    登録項目を無害化する関数を追加。

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
     require_once('Models/Todo.php');
+    require_once('function.php');
 
     // Todoクラスのインスタンス化
     $todo = new Todo();
@@ -24,71 +25,81 @@
 </head>
 <body>
 <header class="px-5 bg-primary">
-        <nav class="navbar navbar-dark">
-            <a href="index.php" class="navbar-brand">TODO APP</a>
-            <div class="justify-content-end">
-                <span class="text-light">
-                    SeedKun
-                </span>
-            </div>
-        </nav>
-    </header>
-    <main class="container py-5">
-        <section>
-            <form class="form-row justify-content-center" action="create.php" method="POST">
-                <div class="col-10 col-md-6 py-2">
-                    <input type="text" class="form-control" placeholder="ADD TODO" name="task">
-                </div>
-                <div class="py-2 col-md-3 col-10">
-                    <button type="submit" class="col-12 btn btn-primary">ADD</button>
-                </div>
-            </form>
-        </section>
-        <section class="mt-5">
-          <table class="table table-hover">
-              <thead>
-                <tr class="bg-primary text-light">
-                    <th class=>TODO</th>
-                    <th>DUE DATE</th>
-                    <th>STATUS</th>
-                    <th></th>
-                    <th></th>
-                </tr>
-                <!-- <tr>
-                    <td>create new website</td>
-                    <td>2019/08/21</td>
-                    <td>NOT YET</td>
-                    <td>
-                        <a class="text-success" href="edit.php">EDIT</a>
-                    </td>
-                    <td>
-                        <a class="text-danger" href="delete.php">DELETE</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>go to club</td>
-                    <td>2019/10/21</td>
-                    <td>DONE</td>
-                    <td>
-                        <a class="text-success" href="edit.php">EDIT</a>
-                    </td>
-                    <td>
-                        <a class="text-danger" href="delete.php">DELETE</a>
-                    </td>
-                </tr> -->
-              </thead>
-              <tbody>
-              <?php foreach ($tasks as
-              $task):?>
-              <tr>
-                <th>
-                <?php echo $task['name']; ?>
-                </th>
+      <nav class="navbar navbar-dark">
+          <a href="index.php" class="navbar-brand">TODO APP</a>
+          <div class="justify-content-end">
+              <span class="text-light">
+                  SeedKun
+              </span>
+          </div>
+      </nav>
+  </header>
+  <main class="container py-5">
+      <section>
+          <form class="form-row justify-content-center" action="create.php" method="POST">
+              <div class="col-10 col-md-6 py-2">
+                  <input type="text" class="form-control" placeholder="ADD TODO" name="task">
+              </div>
+              <div class="py-2 col-md-3 col-10">
+                  <button type="submit" class="col-12 btn btn-primary">ADD</button>
+              </div>
+          </form>
+      </section>
+      <section class="mt-5">
+        <table class="table table-hover">
+            <thead>
+              <tr class="bg-primary text-light">
+                  <th class=>TODO</th>
+                  <th>DUE DATE</th>
+                  <th>STATUS</th>
+                  <th></th>
+                  <th></th>
               </tr>
-              <?php endforeach; ?>
-            </tbody>
-          </table>
-        </section>
-    </main>
+              <!-- <tr>
+                  <td>create new website</td>
+                  <td>2019/08/21</td>
+                  <td>NOT YET</td>
+                  <td>
+                      <a class="text-success" href="edit.php">EDIT</a>
+                  </td>
+                  <td>
+                      <a class="text-danger" href="delete.php">DELETE</a>
+                  </td>
+              </tr>
+              <tr>
+                  <td>go to club</td>
+                  <td>2019/10/21</td>
+                  <td>DONE</td>
+                  <td>
+                      <a class="text-success" href="edit.php">EDIT</a>
+                  </td>
+                  <td>
+                      <a class="text-danger" href="delete.php">DELETE</a>
+                  </td>
+              </tr> -->
+            </thead>
+            <tbody>
+            <?php foreach ($tasks as
+            $task):?>
+            <tr>
+              <td>
+              <?php echo h($task['name']); ?>
+              </td>
+              <td>
+              <?php echo h($task['due_date']); ?>
+              </td>
+              <td>NOT YET</td>
+              <td>
+                <a class="text-success" href="">EDIT</a>
+              </td>
+              <td>
+                <a class="text-danger" href="">DELETE</a>
+              </td>
+            </tr>
+            <?php endforeach; ?>
+          </tbody>
+        </table>
+      </section>
+  </main>
 </body>
 </html>
