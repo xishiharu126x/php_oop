65de9a532fd6d62566134832041f99753448b34c
xishiharu126x <ishiharu126@gmail.com>
2019-10-31 10:24:23 +0800

    deleteの完了

--- delete.php
+++ delete.php
@@ -1,6 +1,9 @@
 <?php
+    require_once 'Models/Todo.php';
+
     $id = $_GET['id'];
 
-    require_once 'Models/Todo.php';
     $todo = new Todo();
     $todo->delete($id);
+
+    header('Location: index.php');
