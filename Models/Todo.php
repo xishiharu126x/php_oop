164090d3eb7da60a5532aa2c0f4233247cae41d8
xishiharu126x <ishiharu126@gmail.com>
2019-10-31 10:04:38 +0800

    deleteの追加

--- Models/Todo.php
+++ Models/Todo.php
@@ -37,4 +37,11 @@ class Todo
         $stmt = $this->db_manager->dbh->prepare('UPDATE '.$this->table.' SET name =? WHERE id = ?');
         $stmt->execute([$name,$id]);
     }
+
+    // 削除するためのメソッド
+    public function delete($id)
+    {
+         $stmt = $this->db_manager->dbh->prepare('DELETE FROM '.$this->table.' WHERE id = ?');
+         $stmt->execute([$id]);
+    }
   }
